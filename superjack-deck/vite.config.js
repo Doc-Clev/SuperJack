3937c2ae59cd0ec649d303311bcd69eb13a65967
Doc-Clev <dave@apservant.com>
2026-04-28 05:35:28 -0700

    Delete superjack-deck directory

--- superjack-deck/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Relative base so it works on GitHub Pages under /superjack-deck/ (or any path).
-export default defineConfig({
-  plugins: [react()],
-  base: "./",
-});
